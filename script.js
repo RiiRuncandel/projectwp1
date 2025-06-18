78c4dde37047ac6bdcd6a4ef7ee8cb93b294b14b
RiiRuncandel <nabillaayudiap@gmail.com>
2025-06-18 12:04:55 +0700

    upd

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ let menu = document.querySelector("#menu-icon");
 let navlist = document.querySelector(".navlist");
 
 menu.onclick = () => {
-    menu.classList.toggle("bx-menu");
+    menu.classList.toggle("bx-x");
     navlist.classList.toggle("open");
 };
 
